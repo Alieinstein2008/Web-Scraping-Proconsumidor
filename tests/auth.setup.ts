98bc5b7b1121c7b74d6018c800931e8928054d9a
Arthur Alonso <72287206+Alieinstein2008@users.noreply.github.com>
2026-01-14 20:51:33 +0000

    :wrench: chore: Atualiza arquivo de setup de autenticação

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -7,7 +7,7 @@ const user: UserInformation = {
     credential: process.env.CRENDENTIAL ?? '',
     password: process.env.PASSWORD ?? ''
 }
-const authFile = process.env.FILE;
+const authFile ='playwright/.auth/user.json';
 
 setup('authenticate', async ({ page }) => {
 
