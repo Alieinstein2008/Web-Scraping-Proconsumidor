c21025582a5450d4b8ae33a8cafba21442cad5a3
Arthur Alonso <72287206+Alieinstein2008@users.noreply.github.com>
2026-01-16 20:36:01 +0000

    :goal_net: Class Tratativa Carta

--- lib/definitions.tsx
+++ lib/definitions.tsx
@@ -1,6 +1,49 @@
 export type UserInformation = {
-    credential:string,
-    password:string
+    credential: string,
+    password: string
 }
 
 export type NA = string;
+
+export type Carta = {
+    Fornecedor: string;
+    Data: string;
+    Prazo: string;
+    Resposta: string;
+    Situacao: string;
+    NumeroAtendimento: NA;
+    CodigoFornecedor: string;
+}
+
+export class TratativaCarta {
+    fornecedor: string;
+    data: string;
+    prazo: string;
+    resposta: string;
+    numeroAtendimento: NA;
+    situacao: string;
+    codFornecedor: string;
+
+    constructor(numeroAtendimento: NA, situacao: string, codFornecedor: string, fornecedor: string, data: string, prazo: string, resposta: string) {
+        this.fornecedor = fornecedor;
+        this.data = data;
+        this.prazo = prazo;
+        this.resposta = resposta;
+        this.numeroAtendimento = numeroAtendimento;
+        this.situacao = situacao;
+        this.codFornecedor = codFornecedor;
+    }
+
+    retornaEstrutura() {
+        const obj: Carta = {
+            Fornecedor: this.fornecedor,
+            Data: this.data,
+            Prazo: this.prazo,
+            Resposta: this.resposta,
+            Situacao: this.situacao,
+            NumeroAtendimento: this.numeroAtendimento,
+            CodigoFornecedor: this.codFornecedor
+        };
+        return obj;
+    }
+};
