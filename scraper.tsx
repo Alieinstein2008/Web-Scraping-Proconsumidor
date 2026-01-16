514c4ff6f08821b830e2dcced35b9eb117a5691c
Arthur Alonso <72287206+Alieinstein2008@users.noreply.github.com>
2026-01-16 20:36:35 +0000

    :construction: Progresso scraper

--- scraper.tsx
+++ scraper.tsx
@@ -1,12 +1,12 @@
 // @ts-check
 import playwright from 'playwright';
 import dotenv from 'dotenv';
-import { NA } from './lib/definitions';
+import { NA, TratativaCarta } from './lib/definitions';
 dotenv.config();
 
-const numeroAtendimento: NA = (process.env.NA_TESTES?? '').replace(/[^a-zA-Z0-9]/g, '');
+const numeroAtendimento: NA = (process.env.NA_CARTAS2 ?? '').slice(0,22);
 const textoBusca = 'Carta';
-const regexBusca = new RegExp(`/\[${textoBusca} -\/`,'');
+const regexBusca = new RegExp(`[0-9] - ${textoBusca}`, '');
 
 (async () => {
 
@@ -17,17 +17,33 @@ const regexBusca = new RegExp(`/\[${textoBusca} -\/`,'');
         locale: 'pt-BR'
     });
     const page = await context.newPage();
-    console.log(numeroAtendimento.slice(0,17))
-    await page.goto('https://proconsumidor.mj.gov.br/#/inicio', {waitUntil:'networkidle'});
+
+    await page.goto('https://proconsumidor.mj.gov.br/#/inicio', { waitUntil: 'networkidle' });
     await page.getByPlaceholder('Nº de Atendimento').fill(numeroAtendimento);
     await page.getByTitle('Pesquisar').click();
-    await page.waitForURL(`https://proconsumidor.mj.gov.br/#/reclamacao/pesquisa/${numeroAtendimento.slice(0,17)}`);
-    await page.waitForSelector('.loader-container',{state:'hidden'});
-    for (const correspondente of await page.getByText()) {
-        
-    }
-    await page.screenshot({ path: `example-${playwright.chromium.name()}.png` });
+    await page.waitForURL(`https://proconsumidor.mj.gov.br/#/reclamacao/pesquisa/${(numeroAtendimento.replace(/[^a-zA-Z0-9]/g, ''))}`);
+    await page.waitForSelector('.loader-container', { state: 'hidden' });
 
+    const lista = [];
+
+    for (const correspondencia of await page.getByText(regexBusca).all()) {
+        await correspondencia.click();
+        await page.waitForSelector('.loader-container', { state: 'hidden' });
+        for (const linha of await page.locator('app-tratativa table tbody tr:nth-child(n)').all()) {
+            const conteudo: string[] = await linha.locator('> td').allInnerTexts();
+            const args: [string, string, string, string] = conteudo as [string, string, string, string];
+            const situacao = await page.locator('app-tratativa span').filter({ hasText: /(Finalizada|Cancelada|Aberta)/ }).textContent() ?? '';
+            const codFornecedor = await page.getByText(args[0]).all();
+            //await page.locator('app-reclamacao-fornecedor').filter({hasText:args[0]}).textContent();
+            //const carta = new TratativaCarta(numeroAtendimento, situacao, 'codFornecedor', ...args);
+            //const obj = carta.retornaEstrutura();
+            //lista.push(obj);
+            console.log(codFornecedor)
+        }
+
+    }
+    //await page.screenshot({ path: `example-${playwright.chromium.name()}.png` });
+    //console.log(lista)
     await browser.close();
 
 })();
