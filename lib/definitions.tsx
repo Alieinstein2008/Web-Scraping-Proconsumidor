9aa5f61ed028a29d4eac4cef9d038fe3e3cc29eb
Arthur Alonso <arthuralonso8356@gmail.com>
2026-02-02 00:45:08 -0300

    :sparkles:	feat: Classe NumeroAtendimento

--- lib/definitions.tsx
+++ lib/definitions.tsx
@@ -1,32 +1,50 @@
 export type UserInformation = {
     credential: string,
-    password: string
+    password: string 
 }
 
-export type NA = string;
-
-export type Carta = {
-    Fornecedor: string;
-    Data: string;
-    Prazo: string;
-    Resposta: string;
-    Situacao: string;
-    NumeroAtendimento: NA;
-    CodigoFornecedor: string;
-    CNPJ: string;
+export type EstruturaCarta = {
+    Fornecedor?: string;
+    Data?: string;
+    Prazo?: string;
+    Resposta?: string;
+    Situacao?: string;
+    NumeroAtendimento?: string;
+    CodigoFornecedor?: string;
+    CNPJ?: string;
 }
+
+export class NumeroAtendimento {
+    numeroAtendimento: string;
+
+    constructor(numeroAtendimento: string) {
+        this.numeroAtendimento = numeroAtendimento;
+    }
+
+    Formatacao(tipo: number) {
+        switch (tipo) {
+            case 1:
+                return this.numeroAtendimento.slice(0, 22);
+            case 2:
+                return this.numeroAtendimento.slice(0, 22).replace(/[^a-zA-Z0-9]/g, "");
+            default:
+                return this.numeroAtendimento;
+        }
+    }
+}
+
 
 export class TratativaCarta {
     fornecedor: string;
     data: string;
     prazo: string;
     resposta: string;
-    numeroAtendimento: NA;
+    numeroAtendimento: string;
     situacao: string;
     codFornecedor: string;
     cnpj: string;
 
-    constructor(numeroAtendimento: NA, situacao: string, codFornecedor: string, cnpj: string, fornecedor: string, data: string, prazo: string, resposta: string) {
+    constructor(numeroAtendimento: string, situacao: string, codFornecedor: string, cnpj: string, fornecedor: string, data: string, prazo: string, resposta: string) {
         this.fornecedor = fornecedor;
         this.data = data;
         this.prazo = prazo;
@@ -40,7 +58,7 @@ export class TratativaCarta {
     retornaEstrutura(tipo: number) {
         switch (tipo) {
             default:
-                const estrutura: Carta = {
+                const estrutura: EstruturaCarta = {
                     NumeroAtendimento: this.numeroAtendimento,
                     CodigoFornecedor: this.codFornecedor,
                     Fornecedor: this.fornecedor,
