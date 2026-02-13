c9c9f21fc17a0aff30db1ccf0b645f696c1b8839
Arthur Alonso <arthuralonso8356@gmail.com>
2026-02-13 16:47:07 -0300

    :sparkles: feat: Add method class BaseDados

--- lib/definitions.tsx
+++ lib/definitions.tsx
@@ -1,4 +1,4 @@
-import { extrairBaseCompleta, extrairColunaBase, atualizarBase } from "./functions";
+import { extrairBaseCompleta, extrairColunaBase, atualizarBase, extrairDivergenciasColunaBaseComparativa, extrairDadosBasePorValorColuna } from "./functions";
 
 export type UserInformation = {
     credential: string,
@@ -35,7 +35,6 @@ export class NumeroAtendimento {
         }
     }
 }
-
 
 export class TratativaCarta {
     fornecedor: string;
@@ -88,25 +87,47 @@ export class BaseDados {
 
     constructor(caminho: string) {
         this.caminho = caminho;
-        this.obterDadosGerais = extrairBaseCompleta.bind(this);
+        this.obterDadosGerais = extrairBaseCompleta.bind({ caminho: this.caminho });
         this.obterDadosColuna = extrairColunaBase.bind({ base: this.obterDadosGerais() });
+        this.obterDadosDivergentes = extrairDivergenciasColunaBaseComparativa.bind({ basePrimaria: this })
         this.carregarAlteracoes = atualizarBase.bind({ base: this.obterDadosGerais() });
+        this.criarFiltroColunaBase = extrairDadosBasePorValorColuna.bind({ base: this.obterDadosGerais() })
 
     }
 
     //Extração
     public obterDadosGerais: () => any[];
     public obterDadosColuna: (coluna: string) => any[];
+    public obterDadosDivergentes: ({ colunaHomologa, baseComparativa }: { colunaHomologa: string, baseComparativa: BaseDados }) => any[];
+    public criarFiltroColunaBase: ({ }: { colunaFiltro: string, valorFiltro: string, colunaRetorno?: string }) => any[];
 
     //Transformação
-    public filtrar(coluna: string): this {
+    public selecionar(coluna: string): this {
         this.baseModificada = this.obterDadosColuna(coluna);
         return this;
     }
 
-    public removerDuplicatas(): this {
-        this.baseModificada = [...new Set(this.baseModificada)];
+    public tipoNumeroAtendimento(tipo: 'Reclamacao' | 'Consulta' | 'Atendimento'): this {
+        switch (tipo) {
+            case "Consulta":
+                this.baseModificada = this.baseModificada.filter(dado => dado.slice(21, 22) == '1');
+                break;
+            case 'Atendimento':
+                this.baseModificada = this.baseModificada.filter(dado => dado.slice(21, 22) == '2');
+                break;
+            case 'Reclamacao':
+                this.baseModificada = this.baseModificada.filter(dado => dado.slice(21, 22) == '3');
+                break;
+            default:
+                this.baseModificada = this.baseModificada.filter(dado => dado.slice(21, 22) == '3');
+                break;
+        }
         return this;
+    }
+
+    public removerDuplicatas(): string[] {
+        this.baseModificada = [...new Set(this.baseModificada)];
+        return this.baseModificada;
     }
 
     public quantificarDados(): number {
@@ -115,7 +136,6 @@ export class BaseDados {
 
     //Carga
     public carregarAlteracoes: (novosDados: any, nomeArquivo: string, nomeAba: string) => void;
-
 
 }
 
@@ -129,7 +149,7 @@ export class Calendario {
     }
 
     public subtrairMeses(meses: number) {
-        
+
         const data = this.dataAtual;
         const dia = data.getDate();
 
@@ -148,7 +168,6 @@ export class Calendario {
         data.setDate(Math.min(dia, ultimoDia));
 
         return new Date(data).toLocaleDateString();
-
     }
 
 }
