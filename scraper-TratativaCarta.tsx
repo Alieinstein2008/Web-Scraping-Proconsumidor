166604c4c598d122efeef9850852dc5e5fa84c61
Arthur Alonso <arthuralonso8356@gmail.com>
2026-02-18 00:05:30 -0300

    :recycle: refactor: Melhora no código do Scraper Tratativa carta

---
+++ scraper-TratativaCarta.tsx
@@ -0,0 +1,109 @@
+import playwright from 'playwright';
+import { NumeroAtendimento, TratativaCarta } from './lib/definitions';
+import { carregarAlteracoesBaseCartas, executarBackupBaseCartas, retornaReclamacoesDivergentes, retornaReclamacoesFalhas, retornaReclamacoesUltimos4Meses } from './utils/database.quickAccessFunctions';
+
+const textoBusca = 'Carta';
+const regexBusca = new RegExp(`[0-9] - ${textoBusca}`, '');
+const regexSituacao = new RegExp(`(Finalizada|Cancelada|Aberta)`, 'i');
+
+let passed = [];
+let allTested = [];
+let failed = [];
+var cont = 0;
+
+(async () => {
+
+    console.time("Tempo-de-Execução-Total");
+    const browser = await playwright.chromium.launch();
+    const context = await browser.newContext({
+        storageState: 'playwright/.auth/user.json',
+        timezoneId: 'America/Sao_Paulo',
+        locale: 'pt-BR'
+    });
+    const page = await context.newPage();
+
+
+    const reclamacoesDivergentes = retornaReclamacoesDivergentes();
+    const grupoBusca = [...reclamacoesDivergentes];
+    const listaBusca = [...new Set(grupoBusca)];
+
+    executarBackupBaseCartas();
+
+    for (const NA of listaBusca) {
+
+        try {
+
+            const inicio = performance.now();
+
+            if (cont == 0) await page.goto('https://proconsumidor.mj.gov.br/#/inicio', { waitUntil: 'networkidle' });
+
+            const numeroAtendimento = new NumeroAtendimento(NA);
+
+            try {
+                await page.getByPlaceholder('Nº de Atendimento').fill(numeroAtendimento.Formatacao(1));
+                await page.getByTitle('Pesquisar').click();
+                await page.waitForURL(`https://proconsumidor.mj.gov.br/#/reclamacao/pesquisa/${numeroAtendimento.Formatacao(2)}`, { timeout: 60000 });
+                await page.waitForSelector('.loader-container', { state: 'hidden' });
+
+                //Abre o painel suspenso de tratativas caso esteja fechado/recolhido
+                const painelTratativa = page.locator('app-tratativa');
+                if (!await painelTratativa.isVisible()) {
+                    await page.getByTitle('Clique para Expandir/Recolher').filter({ hasText: "Tratativas" }).click();
+                }
+
+                for (const correspondencia of await page.getByText(regexBusca).all()) {
+
+                    await correspondencia.click({ timeout: 60000 });
+                    await page.waitForSelector('.loader-container', { state: 'hidden' });
+                    const situacaoCarta = await page.locator('app-tratativa span').filter({ hasText: regexSituacao }).textContent() ?? '';
+
+                    const tabelaCarta = await page.locator('app-tratativa table tbody tr:nth-child(n)').all()
+                    for (const linha of tabelaCarta) {
+
+                        const conteudoLinha: string[] = await linha.locator('> td').allInnerTexts();
+                        const args: [string, string, string, string] = conteudoLinha as [string, string, string, string];
+                        const fornecedor = page.locator('app-reclamacao-fornecedor div.row.mb-2').filter({ has: page.locator('div.col-md-3', { hasText: args[0] }) });
+                        const fornecedorCodigo = (await fornecedor.locator('input').nth(1).inputValue()).slice(22, 24);
+                        const fornecedorCNPJ = await fornecedor.locator('input').nth(3).inputValue();
+                        const carta = new TratativaCarta('passed', numeroAtendimento.Formatacao(1), situacaoCarta, fornecedorCodigo, fornecedorCNPJ, ...args);
+                        const estrutura = carta.retornaEstrutura(1);
+                        passed.push(estrutura);
+                        allTested.push(estrutura);
+
+                    }
+                }
+
+                const fim = performance.now();
+                const time = ((fim - inicio) / 1000).toFixed(2);
+                console.log(`NA: ${numeroAtendimento.Formatacao(1)} Tempo de Execução: ${time}s  ✅  ${cont + 1}°`);
+                cont++;
+
+            } catch (error) {
+                const fim = performance.now();
+                const time = ((fim - inicio) / 1000).toFixed(2);
+                const argsFailed: [string, string, string, string, string, string, string] = ['', '', '', '', '', '', ''];
+                const cartaFailed = new TratativaCarta('failed', numeroAtendimento.Formatacao(1), ...argsFailed,);
+                const estruturaFailed = cartaFailed.retornaEstrutura(1);
+                console.log(`NA: ${numeroAtendimento.Formatacao(1)} Tempo de Execução: ${time}s ❌  ${cont + 1}°`);
+                failed.push(estruturaFailed);
+                allTested.push(estruturaFailed);
+                cont++;
+                continue;
+            }
+        } catch (error) {
+
+            carregarAlteracoesBaseCartas(allTested);
+            console.log('\nPlanilhas Geradas com erro! 🔺\n');
+            console.timeEnd("Tempo-de-Execução-Total");
+            break;
+
+        }
+
+    }
+
+    carregarAlteracoesBaseCartas(allTested);
+    console.log('\nPlanilhas Geradas ✅\n');
+    console.timeEnd("Tempo-de-Execução-Total");
+    await browser.close();
+
+})();
