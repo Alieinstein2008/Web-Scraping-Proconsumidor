ce1cb0868b5a020df5fb8fe7e2da38ce7491ecf3
Arthur Alonso <arthuralonso8356@gmail.com>
2026-02-17 23:09:15 -0300

    :sparkles: feat: Add arquivo com funções de fácil acesso e úteis para o scraping de cartas

---
+++ utils/database.quickAccessFunctions.tsx
@@ -0,0 +1,41 @@
+import { baseDadosCartas } from "./database.config";
+
+export function retornaReclamacoesDivergentes() {
+    const reclamacoesDivergentes = baseDadosCartas.atual.obterDadosDivergentes({
+        colunaHomologa: "NumeroAtendimento",
+        baseComparativa: baseDadosCartas.comparativa,
+        tipoNumeroAtendimento: 'Reclamacao'
+    });
+    return reclamacoesDivergentes;
+};
+
+export function retornaReclamacoesFalhas() {
+    const reclamacoesFalhas = baseDadosCartas.atual.criarFiltroColunaBase({
+        colunaFiltro: 'Scraping',
+        valorFiltro: 'failed',
+        colunaRetorno: 'NumeroAtendimento',
+        tipoNumeroAtendimento: 'Reclamacao'
+    });
+    return reclamacoesFalhas;
+};
+
+export function retornaReclamacoesUltimos4Meses() {
+    const reclamacoesUltimos4Meses = baseDadosCartas.atual.selecionar('NumeroAtendimento')
+        .tipoNumeroAtendimento('Reclamacao')
+        .obterRegistrosUltimosMeses({ quantidadeMeses: 4 })
+        .removerDuplicatas();
+    return reclamacoesUltimos4Meses;
+};
+
+export function executarBackupBaseCartas() {
+    baseDadosCartas.atual.executarBackup({
+        nomeArquivo: 'Cartas-Base-Web-Scraping(Backup)',
+        nomeAba: 'Cartas(Backup)'
+    });
+};
+
+export function carregarAlteracoesBaseCartas(data: any) {
+    baseDadosCartas.atual.carregarAlteracoes(
+        data, 'Cartas-Base-Web-Scraping', 'All'
+    )
+}
