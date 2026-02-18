8bc1c0dc864158187c2df7fe8140d8e574f7af97
Arthur Alonso <arthuralonso8356@gmail.com>
2026-02-18 00:11:00 -0300

    :sparkles: feat: Add database.config.tsx

---
+++ utils/database.config.tsx
@@ -0,0 +1,5 @@
+import { BaseDados } from "../lib/definitions";
+export const baseDadosCartas = {
+    atual: new BaseDados('data/Cartas-Base-Web-Scraping.xlsx'),
+    comparativa: new BaseDados('data/Base-Bi-WebScraping-Cartas-NA(Comparativa).xlsx')
+};
