69c881cb2352254d017de5567332135ae6d3ccdd
Arthur Alonso <arthuralonso8356@gmail.com>
2026-03-10 15:06:05 -0300

    :recycle: refactor: Passando a utilizar as funções criadas no filePah.config.ts e melhorando a dinamicidade e facilidade na leitura e escrita de novas configuracoes de database

--- utils/databaseCartas.config.tsx
+++ utils/databaseCartas.config.tsx
@@ -1,14 +1,28 @@
-import { createDirectoriesInRoot, dirnameInput, dirnameOutput } from "../config/directories.config";
+import { setDefaultFilesPath } from "../config/filePath.config";
 import { BaseDados } from "../lib/definitions";
 
-(async () => {
-    await createDirectoriesInRoot({
-        dirnameInput: dirnameInput,
-        dirnameOutput: dirnameOutput
-    });
-})();
+let inputPathCartas: string = '';
+let outputPathCartas: string = '';
+let filesPathCartas: string[] = [];
+
+const config = setDefaultFilesPath({
+    directoryInputRelativePath: '../data/in/Cartas',
+    directoryOutputRelativePath: '../data/out/Cartas',
+    inputFilenames: [
+        'Cartas-Base-Web-Scraping.xlsx',
+        'Cartas-Base-Bi-Comparativa.xlsx'
+    ]
+});
+
+if (config.sucess) {
+    inputPathCartas = config.directoryInputPath;
+    outputPathCartas = config.directoryOutputPath;
+    filesPathCartas = config.filesPath;
+};
+
+export { inputPathCartas, outputPathCartas, filesPathCartas };
 
 export const baseDadosCartas = {
-    atual: new BaseDados(`${dirnameInput}/Cartas/Cartas-Base-Web-Scraping.xlsx`),
-    comparativa: new BaseDados(`${dirnameInput}/Cartas/Cartas-Base-Bi-Comparativa.xlsx`)
+    atual: new BaseDados(filesPathCartas[0]),
+    comparativa: new BaseDados(filesPathCartas[1])
 };
