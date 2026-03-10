80825433733ca637b6e181720434942491debd1a
Arthur Alonso <arthuralonso8356@gmail.com>
2026-03-09 22:19:43 -0300

    :goal_net: Tratando erros referentes a sincronicidade e a localização de caminhos corretos e relatividade seguindo uma arvore de arquivos dinaminca

--- config/filePath.config.ts
+++ config/filePath.config.ts
@@ -1,50 +1,66 @@
 import fs from 'fs';
 import xlsx from 'xlsx';
 import path from 'path';
+import callerPath from 'caller-path';
 
 interface CreateError {
     type: 'createDirectoryError' | 'createFileError',
     message: string
-}
+};
 
-export async function createDirectories({ directoriesRelativePath }: { directoriesRelativePath: string[] }): Promise<{ sucess: true; } | { sucess: false; error: CreateError; }> {
-    let coutErrors = 0;
+export function createDirectories({ directoriesRelativePath }: { directoriesRelativePath: string[] }): { sucess: true; } | { sucess: false; error: CreateError; } {
+
+    let errorsCount = 0;
     for (const directoryRelativePath of directoriesRelativePath) {
         try {
-            fs.mkdir(directoryRelativePath, { recursive: true }, (err) => {
-                if (err) throw err;
-            });
+            fs.mkdirSync(directoryRelativePath, { recursive: true });
         } catch (err) {
-            coutErrors++;
+            errorsCount++;
         }
     };
 
-    if (coutErrors > 0) return {
+    if (errorsCount > 0) return {
         sucess: false,
-        error: { type: 'createDirectoryError', message: `Falha um ou mais diretórios` }
+        error: { type: 'createDirectoryError', message: `Falha na criação de um ou mais diretórios` }
     };
     return { sucess: true };
 
 };
 
-export async function setDefaultFilesPath({ directoryInputRelativePath, directoryOutputRelativePath, filenamesInput }: { directoryInputRelativePath: string, directoryOutputRelativePath: string, filenamesInput: string[] }) {
+export function setDefaultFilesPath(
+    { directoryInputRelativePath, directoryOutputRelativePath, filenamesInput }: { directoryInputRelativePath: string, directoryOutputRelativePath: string, filenamesInput: string[] }): string[] {
 
-    const inputPath = path.join(__dirname, directoryInputRelativePath);
-    const outputPath = path.join(__dirname, directoryOutputRelativePath);
-    const createResponse = await createDirectories({ directoriesRelativePath: [inputPath, outputPath] });
-    if (createResponse.sucess) {
+    const callPath = callerPath();
+
+    const inputPath = callPath !== undefined ? path.join(path.dirname(callPath), directoryInputRelativePath) : path.join(__dirname, directoryInputRelativePath);
+    const outputPath = callPath !== undefined ? path.join(path.dirname(callPath), directoryOutputRelativePath) : path.join(__dirname, directoryOutputRelativePath);
+
+    const response = createDirectories({ directoriesRelativePath: [inputPath, outputPath] });
+
+    if (response.sucess) {
+
         for (const filenameInput of filenamesInput) {
-            const fileInputPath = path.join(inputPath, filenameInput);
+
+            const fileInputPath = path.resolve(inputPath, filenameInput);
+
             if (!fs.existsSync(fileInputPath)) {
+
                 const dadosNulosJson = [{}];
                 const worksheet = xlsx.utils.json_to_sheet(dadosNulosJson);
                 const workbook = xlsx.utils.book_new();
                 xlsx.utils.book_append_sheet(workbook, worksheet);
                 xlsx.writeFile(workbook, fileInputPath);
-                console.log(`${filenameInput} criado com sucesso📄`)
-            }else{
-                console.log(`${filenameInput} já existente📄`)
-            }
-        }
+                console.log(`${fileInputPath} gerado com sucesso📄✅`);
+
+            } else {
+                console.log(`${filenameInput} já existente📄✅`);
+            };
+        };
+
+        return [inputPath, outputPath];
+
+    } else {
+        console.log(response.error.message);
+        return ['', '']
     }
 };
