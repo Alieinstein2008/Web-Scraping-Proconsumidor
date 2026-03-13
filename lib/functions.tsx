7225bdfc1a2c66472e31fcff083de28fd37cdeb5
Arthur Alonso <arthuralonso8356@gmail.com>
2026-03-13 15:02:51 -0300

    :recycle: refactor: Utilizando o method pop()  mais eficiente e semantico para acessar o item mais recente do array novos dados

--- lib/functions.tsx
+++ lib/functions.tsx
@@ -2,8 +2,7 @@ import xlsx from 'xlsx';
 import { BaseDados, Calendario, TipoNumeroAtendimento } from './definitions';
 
 export function atualizarBase(this: { base: any[] }, { novosDados, nomeArquivo, nomeAba, inputPath }: { novosDados: any[], nomeArquivo: string, nomeAba: string, inputPath: string }): void {
-    
-    const novaEstrutura = novosDados[novosDados.length - 1];
+    const novaEstrutura = novosDados.pop();
     this.base.push(novaEstrutura);
     const worksheet = xlsx.utils.json_to_sheet(this.base);
     const workbook = xlsx.utils.book_new();
