0a878d0d9dc78a4e6bacb2293d14a0287c142e48
Eduardo <eduardo.maia@estudante.ufjf.br>
2026-03-19 12:09:35 -0300

    :white_check_mark:test: Create a test to extract data from a date.

---
+++ config/testeAudiencia.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from "@playwright/test";
+import playwright from "playwright";
+import {customContext, customOptimizationBrowserArgsLaunch, customOptimizationPageRoute, customRefreshPage, customTimeout} from "./customDefinitions.config";
+import { NumeroAtendimento } from "../lib/definitions";
+test("test", async ({ page }) => {
+  console.time("Tempo-de-Execução-Total");
+
+  const browser = await playwright.chromium.launch({args: customOptimizationBrowserArgsLaunch,});
+
+  const context = await customContext(browser);
+  const regexSituacao = new RegExp(`(Finalizada|Cancelada|Aberta)`, 'i');
+  await page.goto("https://proconsumidor.mj.gov.br/#/inicio", {waitUntil: "networkidle", timeout: customTimeout.general});
+
+  //const itensBusca = "22.12.0532.001.00701-3";
+  //for (const NA of itensBusca) {
+    const numeroAtendimento = new NumeroAtendimento("22.12.0532.001.00701-3");
+    try {
+      await page.getByPlaceholder("Nº de Atendimento").fill(numeroAtendimento.Formatacao(1));
+      await page.getByTitle("Pesquisar").first().click({ timeout: 30000 });
+      await page.waitForURL(`https://proconsumidor.mj.gov.br/#/reclamacao/pesquisa/${numeroAtendimento.Formatacao(2)}`,{ timeout: customTimeout.general});
+      await page.waitForSelector(".loader-container", { state: "hidden" });
+
+      const data= await page.locator('app-tratativa h5').first().textContent();
+      
+     
+      
+    } catch {}
+  //}
+});
