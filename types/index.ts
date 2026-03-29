d107618cb6dedd34f9d7d3420b19c52f0b6cd919
Arthur Alonso <arthuralonso8356@gmail.com>
2026-03-29 16:12:33 -0300

    :wrench: chore: Adicionando arquivo centralizador dos types index.ts no diretorio types/

---
+++ types/index.ts
@@ -0,0 +1,22 @@
+import { EstruturaCarta, TuplaInformacoesFailedCarta, TuplaInformacoesNulasCarta, TuplaInformacoesParciaisCarta } from "./carta.types";
+import { EstruturaConsumidor, TuplaInfomacoesNulasConsumidor, TuplaInformacoesFailedConsumidor, TuplaInformacoesParciaisConsumidorPessoaFisica, TuplaInformacoesParciaisConsumidorPessoaJuridica } from "./consumidor.types";
+import { TipoNumeroAtendimento } from "./numeroAtendimento.types";
+import { UserInformation } from "./user.config.types";
+
+
+export {
+    EstruturaCarta,
+    EstruturaConsumidor,
+    TipoNumeroAtendimento,
+    TuplaInformacoesFailedCarta,
+    TuplaInformacoesNulasCarta,
+    TuplaInformacoesParciaisCarta,
+    TuplaInfomacoesNulasConsumidor,
+    TuplaInformacoesFailedConsumidor,
+    TuplaInformacoesParciaisConsumidorPessoaFisica,
+    TuplaInformacoesParciaisConsumidorPessoaJuridica,
+    UserInformation
+};
+
+
+
