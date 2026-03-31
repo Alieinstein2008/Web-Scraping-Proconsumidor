dd07af342c8e4b37ae51f10c60a411bc9d7d48e7
Arthur Alonso <arthuralonso8356@gmail.com>
2026-03-31 17:59:37 -0300

    :bug: fix: Solucionando logica com operador spread

--- lib/functions.tsx
+++ lib/functions.tsx
@@ -3,7 +3,7 @@ import { BaseDados, Calendario } from './definitions';
 import { TipoNumeroAtendimento } from '../types/index';
 
 export function atualizarBase(this: { base: any[] }, { novosDados, nomeArquivo, nomeAba, inputPath }: { novosDados: any[], nomeArquivo: string, nomeAba: string, inputPath: string }): void {
-    this.base.push(novosDados);
+    this.base.push(...novosDados);
     const worksheet = xlsx.utils.json_to_sheet(this.base);
     const workbook = xlsx.utils.book_new();
     xlsx.utils.book_append_sheet(workbook, worksheet, `${nomeAba}`);
