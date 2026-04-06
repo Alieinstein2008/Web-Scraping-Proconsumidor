938f5164fc5abc5660000ebe16400b229f760da4
Eduardo Maia <eduardomaia02122006@gmail.com>
2026-04-05 23:17:47 -0300

    :sparkles: feat: Criando função de reclamações divergentes de um período

--- utils/databaseAudiencia.quickAcessFunctions.tsx
+++ utils/databaseAudiencia.quickAcessFunctions.tsx
@@ -2,6 +2,17 @@ import { baseDadosAudiencia} from  "./databaseAudiencia.config";
 import { inputPathAudiencia, outputPathAudiencia } from "./databaseAudiencia.config";
 import { Calendario } from "../lib/definitions";
 
+export function retornaReclamacoesDivergentesPeriodo({ dataInicial, dataFinal }: { dataInicial?: string, dataFinal?: string }): any[] {
+    const reclamacoesDivergentes = baseDadosAudiencia.atual.obterDadosDivergentes({
+        colunaHomologa: "NumeroAtendimento",
+        baseComparativa: baseDadosAudiencia.comparativa,
+        tipoNumeroAtendimento: 'Reclamacao',
+        dataInicial: dataInicial,
+        dataFinal: dataFinal
+    });
+    return reclamacoesDivergentes;
+};
+
 export function executarBackupBaseAudiencia(): void {
     const prefixoArquivo = new Calendario().prefixoArquivoDataAtual();
     baseDadosAudiencia.atual.executarBackup({
