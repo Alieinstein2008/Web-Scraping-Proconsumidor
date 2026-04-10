b0762c6e217a2e56102db78e9063162f624b2b49
Eduardo Maia <eduardomaia02122006@gmail.com>
2026-04-10 12:24:25 -0300

    :recycle: refactor: Simplifica a atualização da base ao adicionar novos dados

--- lib/functions.tsx
+++ lib/functions.tsx
@@ -2,8 +2,8 @@ import xlsx from 'xlsx';
 import { BaseDados, Calendario, TipoNumeroAtendimento } from './definitions';
 
 export function atualizarBase(this: { base: any[] }, { novosDados, nomeArquivo, nomeAba, inputPath }: { novosDados: any[], nomeArquivo: string, nomeAba: string, inputPath: string }): void {
-    const novaEstrutura = novosDados.pop();
-    this.base.push(novaEstrutura);
+
+    this.base.push(...novosDados);
     const worksheet = xlsx.utils.json_to_sheet(this.base);
     const workbook = xlsx.utils.book_new();
     xlsx.utils.book_append_sheet(workbook, worksheet, `${nomeAba}`);
