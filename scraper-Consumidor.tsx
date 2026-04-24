daa8c1c89ceb95126d26b328d1be56a0cb19acd5
Arthur Alonso <arthuralonso8356@gmail.com>
2026-04-24 17:45:55 -0300

    :construction: Alteracoes de revisao necessarias ainda em processo

--- scraper-Consumidor.tsx
+++ scraper-Consumidor.tsx
@@ -3,7 +3,7 @@ import { createLogger } from './config/loggers.config';
 import { coordenadasCep } from './config/fetchApi.config';
 import { TuplaInfomacoesNulasConsumidor, TuplaInformacoesFailedConsumidor, TuplaInformacoesParciaisConsumidorPessoaJuridica, TuplaInformacoesParciaisConsumidorPessoaFisica } from './types/index';
 import { customContext, customOptimizationBrowserArgsLaunch, customOptimizationPageRoute, customRefreshPage, customTimeout } from './config/customDefinitions.config';
-import { carregarAlteracoesBaseConsumidorBairrosRegionais, numerosAtendimentosBairrosRegionais } from './utils/databaseConsumidor.quickAcessFunctions';
+import { carregarAlteracoesBaseConsumidorBairrosSuperendividamento, numerosAtendimentosBairrosSuperendividamento } from './utils/databaseConsumidor.quickAcessFunctions';
 import { ConsumidorPessoaFisica, ConsumidorPessoaJuridica, NumeroAtendimento } from './lib/definitions';
 
 let allTested = [];
@@ -29,7 +29,7 @@ const logger = createLogger({
 
     let page = await context.newPage();
 
-    const itensBusca = numerosAtendimentosBairrosRegionais;
+    const itensBusca = numerosAtendimentosBairrosSuperendividamento;
 
     for (const NA of itensBusca) {
 
@@ -67,71 +67,81 @@ const logger = createLogger({
                     await page.locator('div').filter({ hasText: regexTextPainelExtensivel }).nth(1).click({ timeout: customTimeout.click });
                 }
 
-                await painelExpansivel().locator('div.sub-titulo', { hasText: 'Consumidor' }).waitFor({ state: 'visible' })
-                const dropdownMenu = painelExpansivel().getByRole('button').first();
-
-                const tipoIdentidadeConsumidor: 'Anônimo' | string = await painelExpansivel().getByRole('textbox', { name: 'Nome do Consumidor' }).inputValue();
-
-                if (tipoIdentidadeConsumidor === 'Anônimo') {
-                    const argsBlank = Array(14).fill('') as TuplaInfomacoesNulasConsumidor;
-                    const consumidorBlank = new ConsumidorPessoaFisica('blank', numeroAtendimento.Formatacao(1), 'Anônimo', ...argsBlank);
-                    const estruturaConsumidorBlank = consumidorBlank.retornaEstrutura(1);
-                    allTested.push(estruturaConsumidorBlank);
-                    logger.log('blank', `${numeroAtendimento.Formatacao(1)} 👤 ❔`);
-
-                } else {
-
-                    await dropdownMenu.click();
-                    const botaoDetalhar = page.getByText('Detalhar').first();
-                    await botaoDetalhar.click();
-                    await page.waitForResponse(regexWaitForResponseLastUrlRequest);
-                    await page.waitForSelector('.loader-container', { state: 'hidden' });
-
-                    let naturezaConsumidor: 'Fisica' | 'Juridica' = 'Fisica';
-                    let emojiNaturezaConsumidor: '👤 ✅' | '🏢 ✅' = '👤 ✅';
-
-                    if (await page.locator('app-detalhe-consumidor .modal-body label', { hasText: 'CNPJ' }).isVisible()) {
-                        naturezaConsumidor = 'Juridica';
-                        emojiNaturezaConsumidor = '🏢 ✅';
-                        await page.waitForResponse(regexWaitForResponseLastUrlRequest, { timeout: customTimeout.general });
-                        await page.waitForSelector('.loader-container', { state: 'hidden' });
-                    };
-
-                    const informacoesParciaisConsumidor = async () => {
-                        return await Promise.all((await page.locator('app-detalhe-consumidor .modal-body label + input').all()).map(async informacao => await informacao.inputValue()));
-                    };
-
-                    const argsConsumidor = async () => {
-                        return naturezaConsumidor === 'Fisica' ? await informacoesParciaisConsumidor() as TuplaInformacoesParciaisConsumidorPessoaFisica : await informacoesParciaisConsumidor() as TuplaInformacoesParciaisConsumidorPessoaJuridica;
-                    };
-
-                    const info = await informacoesParciaisConsumidor();
-                    const args = await argsConsumidor();
-
-                    const telefones: string[] = await page.locator('app-telefone table tbody tr:nth-child(n) > td').allInnerTexts();
-                    const telefone: string = telefones.join(' - ');
-                    const cep = naturezaConsumidor === 'Fisica' ? info[6] : info[2];
-                    const [latitude, longitude] = await coordenadasCep(cep);
-                    const consumidor = args.length === 12 ? new ConsumidorPessoaFisica('passed', numeroAtendimento.Formatacao(1), ...args, telefone, latitude, longitude) : new ConsumidorPessoaJuridica('passed', numeroAtendimento.Formatacao(1), ...args, telefone, latitude, longitude);
-                    const estruturaConsumidor = consumidor.retornaEstrutura(1);
-                    allTested.push(estruturaConsumidor);
-                    await page.getByRole('button', { name: 'Close' }).first().click({ timeout: customTimeout.general });
-                    logger.log('passed', `${numeroAtendimento.Formatacao(1)} ${emojiNaturezaConsumidor}`);
-
+                if (await painelExpansivel().getByRole('img').first().isVisible()) {
+                    console.info(`${numeroAtendimento.Formatacao(1)} RECLAMACAO DE OFICIO`)
+                    allTested.push({ NumeroAtendimento: numeroAtendimento.Formatacao(1), Bairro: 'Reclamacao de Oficio' })
                 }
+                 
+                else {
+                    await painelExpansivel().locator('div.sub-titulo', { hasText: 'Consumidor' }).waitFor({ state: 'visible' })
+
+                    const dropdownMenu = painelExpansivel().getByRole('button').first();
+
+                    const tipoIdentidadeConsumidor: 'Anônimo' | string = await painelExpansivel().getByRole('textbox', { name: 'Nome do Consumidor' }).inputValue();
+
+                    if (tipoIdentidadeConsumidor === 'Anônimo') {
+                        const argsBlank = Array(14).fill('') as TuplaInfomacoesNulasConsumidor;
+                        const consumidorBlank = new ConsumidorPessoaFisica('blank', numeroAtendimento.Formatacao(1), 'Anônimo', ...argsBlank);
+                        const estruturaConsumidorBlank = consumidorBlank.retornaEstrutura(1);
+                        allTested.push(estruturaConsumidorBlank);
+                        logger.log('blank', `${numeroAtendimento.Formatacao(1)} 👤 ❔`);
+
+                    } else {
+
+                        await dropdownMenu.click();
+                        const botaoDetalhar = page.getByText('Detalhar').first();
+                        await botaoDetalhar.click();
+                        await page.waitForResponse(regexWaitForResponseLastUrlRequest);
+                        await page.waitForSelector('.loader-container', { state: 'hidden' });
+
+                        let naturezaConsumidor: 'Fisica' | 'Juridica' = 'Fisica';
+                        let emojiNaturezaConsumidor: '👤 ✅' | '🏢 ✅' = '👤 ✅';
+
+                        if (await page.locator('app-detalhe-consumidor .modal-body label', { hasText: 'CNPJ' }).isVisible()) {
+                            naturezaConsumidor = 'Juridica';
+                            emojiNaturezaConsumidor = '🏢 ✅';
+                            await page.waitForResponse(regexWaitForResponseLastUrlRequest, { timeout: customTimeout.general });
+                            await page.waitForSelector('.loader-container', { state: 'hidden' });
+                        };
+
+                        const informacoesParciaisConsumidor = async () => {
+                            return await Promise.all((await page.locator('app-detalhe-consumidor .modal-body label + input').all()).map(async informacao => await informacao.inputValue()));
+                        };
+
+                        const argsConsumidor = async () => {
+                            return naturezaConsumidor === 'Fisica' ? await informacoesParciaisConsumidor() as TuplaInformacoesParciaisConsumidorPessoaFisica : await informacoesParciaisConsumidor() as TuplaInformacoesParciaisConsumidorPessoaJuridica;
+                        };
+
+                        const info = await informacoesParciaisConsumidor();
+                        const args = await argsConsumidor();
+
+                        const telefones: string[] = await page.locator('app-telefone table tbody tr:nth-child(n) > td').allInnerTexts();
+                        const telefone: string = telefones.join(' - ');
+                        const cep = naturezaConsumidor === 'Fisica' ? info[6] : info[2];
+                        const [latitude, longitude] = await coordenadasCep(cep);
+                        const consumidor = args.length === 12 ? new ConsumidorPessoaFisica('passed', numeroAtendimento.Formatacao(1), ...args, telefone, latitude, longitude) : new ConsumidorPessoaJuridica('passed', numeroAtendimento.Formatacao(1), ...args, telefone, latitude, longitude);
+                        const estruturaConsumidor = consumidor.retornaEstrutura(1);
+                        allTested.push(estruturaConsumidor);
+                        await page.getByRole('button', { name: 'Close' }).first().click({ timeout: customTimeout.general });
+                        logger.log('passed', `${numeroAtendimento.Formatacao(1)} ${emojiNaturezaConsumidor}`);
+
+                    }
+                }
+
             }
 
             catch (error) {
-                const argsFailed = Array(14).fill('') as TuplaInformacoesFailedConsumidor;
-                const consumidorFailed = new ConsumidorPessoaFisica('failed', '', '', ...argsFailed);
+                const argsFailed = Array(16).fill('') as TuplaInformacoesFailedConsumidor;
+                const consumidorFailed = new ConsumidorPessoaFisica('failed', numeroAtendimento.Formatacao(1), '', ...argsFailed);
                 const estruturaConsumidorFailed = consumidorFailed.retornaEstrutura(1);
                 allTested.push(estruturaConsumidorFailed);
                 logger.error(error)
+                console.log(allTested)
                 logger.log('failed', `${numeroAtendimento.Formatacao(1)} 👤 ❌`);
                 continue;
             }
 
-            carregarAlteracoesBaseConsumidorBairrosRegionais(allTested);
+            carregarAlteracoesBaseConsumidorBairrosSuperendividamento(allTested);
             allTested.length = 0;
             if (contadorOcorrencia % limiteComparativo === 0) logger.info(`${contadorOcorrencia}/${itensBusca.length} alterações carregadas com sucesso 👌`);
             contadorOcorrencia++;
--- utils/databaseConsumidor.quickAcessFunctions.tsx
+++ utils/databaseConsumidor.quickAcessFunctions.tsx
@@ -1,5 +1,41 @@
 import { baseDadosConsumidor, inputPathConsumidor } from "./databaseConsumidor.config";
+import { Calendario } from "../lib/definitions";
 
+const prefixoData = new Calendario().prefixoArquivoDataAtual();
+
+const reclamacoesBairrosSuperendividamento = baseDadosConsumidor.superendividamento.obterDadosDivergentes({
+    colunaHomologa: 'NumeroAtendimento',
+    tipoNumeroAtendimento: 'Reclamacao',
+    baseComparativa: baseDadosConsumidor.superendividamentoComparativa
+});
+
+const consultasBairrosSuperendividamento = baseDadosConsumidor.superendividamento.obterDadosDivergentes({
+    colunaHomologa: 'NumeroAtendimento',
+    tipoNumeroAtendimento: 'Consulta',
+    baseComparativa: baseDadosConsumidor.superendividamentoComparativa
+});
+
+const denunciasBairrosSuperendividamento = baseDadosConsumidor.superendividamento.obterDadosDivergentes({
+    colunaHomologa: 'NumeroAtendimento',
+    tipoNumeroAtendimento: 'Denuncia',
+    baseComparativa: baseDadosConsumidor.superendividamentoComparativa
+});
+
+export const numerosAtendimentosBairrosSuperendividamento = [
+    ...reclamacoesBairrosSuperendividamento,
+    ...consultasBairrosSuperendividamento,
+    ...denunciasBairrosSuperendividamento
+];
+
+export function carregarAlteracoesBaseConsumidorBairrosSuperendividamento(data: any[]): void {
+    baseDadosConsumidor.superendividamento.carregarAlteracoes({
+        novosDados: data,
+        nomeArquivo: 'Consumidor-Bairros-Superendividamento-Web-Scraping',
+        nomeAba: `${prefixoData}Bairros`,
+        inputPath: inputPathConsumidor
+    })
+};
+/* 
 const reclamacoesBairrosRegionais = baseDadosConsumidor.regionais.obterDadosDivergentes({
     colunaHomologa: 'NumeroAtendimento',
     tipoNumeroAtendimento: 'Reclamacao',
@@ -28,7 +64,7 @@ export function carregarAlteracoesBaseConsumidorBairrosRegionais(data: any[]): v
     baseDadosConsumidor.regionais.carregarAlteracoes({
         novosDados: data,
         nomeArquivo: 'Consumidor-Bairros-Regionais-Web-Scraping',
-        nomeAba: 'Hoje',
+        nomeAba: `${prefixoData}Bairros-Regionais`,
         inputPath: inputPathConsumidor
     })
-};
+}; */
