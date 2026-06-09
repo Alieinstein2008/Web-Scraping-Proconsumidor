cf1cb1724ec17651d6ccd7e1cf27209b0ef0570c
Eduardo Fontainha <eduardo.maia@estudante.ufjf.br>
2026-06-09 19:14:02 -0300

    refactor: Refatora funções de acesso a dados na classe databaseAudiencia para melhorar a modularidade e legibilidade do código

--- utils/databaseAudiencia.quickAcessFunctions.tsx
+++ utils/databaseAudiencia.quickAcessFunctions.tsx
@@ -3,49 +3,58 @@ import { prefixoArquivo } from ".";
 import { baseDadosAudiencia, inputPathAudiencia, outputPathAudiencia } from "./databaseAudiencia.config";
 dotenv.config();
 
-export const organizedMappingAudiencia = {
-    baseDadosPrimaria: baseDadosAudiencia.primaria,
-    baseDadosComparativa: baseDadosAudiencia.comparativa,
-    inputPath: inputPathAudiencia,
-    outputPath: outputPathAudiencia,
-    colunaHomologa: 'NumeroAtendimento',
-    nomeArquivoEntrada: 'Audiencias-Web-Scraping',
-    nomeArquivoBackup: `${prefixoArquivo}Audiencias-Web-Scraping(Backup)`,
-    nomeAba: `${prefixoArquivo}Audiencias`
+export function carregarDadosColunaAudiencia(nomeColuna:string) {
+    return {
+        baseDadosPrimaria: baseDadosAudiencia.primaria,
+        baseDadosComparativa: baseDadosAudiencia.comparativa,
+        inputPath: inputPathAudiencia,
+        outputPath: outputPathAudiencia,
+        colunaHomologa: nomeColuna,
+        nomeArquivoEntrada: 'Audiencias-Web-Scraping',
+        nomeArquivoBackup: `${prefixoArquivo}Audiencias-Web-Scraping(Backup)`,
+        nomeAba: `${prefixoArquivo}Audiencias`
+    };
 }
 
-const reclamacoes = organizedMappingAudiencia.baseDadosPrimaria.obterDadosDivergentes({
-    colunaHomologa: organizedMappingAudiencia.colunaHomologa,
-    tipoNumeroAtendimento: 'Reclamacao',
-    baseComparativa: organizedMappingAudiencia.baseDadosComparativa
-});
+export function obterNumerosAtendimentos(colunaHomologa: string) {
+    const organizedMappingAudiencia = carregarDadosColunaAudiencia(colunaHomologa);
 
-const consultas = organizedMappingAudiencia.baseDadosPrimaria.obterDadosDivergentes({
-    colunaHomologa: organizedMappingAudiencia.colunaHomologa,
-    tipoNumeroAtendimento: 'Consulta',
-    baseComparativa: organizedMappingAudiencia.baseDadosComparativa
-});
+    console.log('Coluna homologada para o scraper de audiência:', organizedMappingAudiencia.colunaHomologa);
 
-const denuncias = organizedMappingAudiencia.baseDadosPrimaria.obterDadosDivergentes({
-    colunaHomologa: organizedMappingAudiencia.colunaHomologa,
-    tipoNumeroAtendimento: 'Denuncia',
-    baseComparativa: organizedMappingAudiencia.baseDadosComparativa
-});
+    const reclamacoes = organizedMappingAudiencia.baseDadosPrimaria.obterDadosDivergentes({
+        colunaHomologa: organizedMappingAudiencia.colunaHomologa,
+        tipoNumeroAtendimento: 'Reclamacao',
+        baseComparativa: organizedMappingAudiencia.baseDadosComparativa
+    });
 
+    const consultas = organizedMappingAudiencia.baseDadosPrimaria.obterDadosDivergentes({
+        colunaHomologa: organizedMappingAudiencia.colunaHomologa,
+        tipoNumeroAtendimento: 'Consulta',
+        baseComparativa: organizedMappingAudiencia.baseDadosComparativa
+    });
 
-export const numerosAtendimentos = [
-    ...reclamacoes,
-    ...consultas,
-    ...denuncias
-];
+    const denuncias = organizedMappingAudiencia.baseDadosPrimaria.obterDadosDivergentes({
+        colunaHomologa: organizedMappingAudiencia.colunaHomologa,
+        tipoNumeroAtendimento: 'Denuncia',
+        baseComparativa: organizedMappingAudiencia.baseDadosComparativa
+    });
+
+    return [
+        ...reclamacoes,
+        ...consultas,
+        ...denuncias
+    ];
+}
+
 
 export function carregarAlteracoes(data: any[]): void {
+    const organizedMappingAudiencia = carregarDadosColunaAudiencia('NumeroAtendimento');
     organizedMappingAudiencia.baseDadosPrimaria.carregarAlteracoes({
         novosDados: data,
         nomeArquivo: organizedMappingAudiencia.nomeArquivoEntrada,
         nomeAba: organizedMappingAudiencia.nomeAba,
         inputPath: organizedMappingAudiencia.inputPath
-    })
+    });
 };
 
 export function salvarAlteracoes(signal: string, data: any[]): void {
@@ -56,6 +65,7 @@ export function salvarAlteracoes(signal: string, data: any[]): void {
 };
 
 export function executarBackup() {
+    const organizedMappingAudiencia = carregarDadosColunaAudiencia('NumeroAtendimento');
     console.log('Iniciando processo de backup da base de dados primária ⏳');
     organizedMappingAudiencia.baseDadosPrimaria.executarBackup({
         nomeArquivo: organizedMappingAudiencia.nomeArquivoBackup,
@@ -64,3 +74,13 @@ export function executarBackup() {
     });
     console.log('Backup concluído com sucesso. Finalizando processo.');
 }
+
+const databaseAudienciaQuickAccess = {
+    carregarDadosColunaAudiencia,
+    obterNumerosAtendimentos,
+    carregarAlteracoes,
+    salvarAlteracoes,
+    executarBackup
+};
+
+export default databaseAudienciaQuickAccess;
